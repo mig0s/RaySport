bb232c3b0b44829d0241ff5710f5a825d2acdc0a
Mikhail Gospodarikov <mail@mig0s.tk>
2016-06-23 06:37:20 +0800

    Initial commit

---
+++ src/test2/Test2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test2;
+
+/**
+ *
+ * @author SYSTEM
+ */
+public class Test2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Main  man=new Main();
+        man.setVisible(true);
+    }
+    
+}
